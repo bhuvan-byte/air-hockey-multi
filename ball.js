e57d9edf5cad7bc8648eec094079797a8aca84bf
bhuvan <bhuvanaggarwal1@gmail.com>
2020-09-06 14:29:10 +0530

    Added collision between balls

--- ball.js
+++ ball.js
@@ -1,13 +1,44 @@
 function Ball(x,y,radius){
     this.x=x;
     this.y=y;
-    this.vx=-1;
-    this.vy=2;
+    this.vx=-Math.random()*4;
+    this.vy=Math.random()*5;
     this.radius=radius;
+    this.friction=1;
+    this.mass=1;
+    this.color='#000';
     this.draw = function(ctx){
         ctx.beginPath();
         ctx.arc(this.x,this.y,this.radius,0,2*Math.PI);
+        ctx.strokeStyle=(this.color);
         ctx.stroke();
+    }
+    this.collide = function(ball2){
+        let dx=ball2.x-this.x,
+			dy=ball2.y-this.y,
+			radSum=ball2.radius+this.radius;
+		if(dx*dx + dy*dy< radSum*radSum){
+			let dist=Math.sqrt(dx*dx + dy*dy),
+				dif=radSum-dist;
+			dx/=dist;
+			dy/=dist;
+			let dot=dx*this.vx+dy*this.vy,
+				dot2=dx*ball2.vx+dy*ball2.vy,
+				impulsex=(dot-dot2)*dx,
+				impulsey=(dot-dot2)*dy;
+			//console.log(dot,dot2,impulsex,impulsey);
+			this.vx-=impulsex;
+			this.vy-=impulsey;
+			ball2.vx+=impulsex;
+			ball2.vy+=impulsey;
+			this.x-=dif*dx;
+			this.y-=dif*dy;
+			ball2.x+=dif*dx;
+			ball2.y+=dif*dy;
+			//this.dx+=dif*dx/2;
+			//this.dy+=dif*dy/2;
+			//if(this.radius>10)this.radius-=5;
+        }
     }
     this.update= function(){
         if(this.x -this.radius< 0 || this.x+this.radius>width){
@@ -18,5 +49,7 @@ function Ball(x,y,radius){
         }
         this.x+=this.vx;
         this.y+=this.vy;
+        this.vx*=this.friction;
+        this.vy*=this.friction;
     }
 }
--- game.js
+++ game.js
@@ -1,16 +1,19 @@
 window.onload= function(){
     var canvas=document.getElementById("canvas");
-    width=canvas.width=window.innerWidth;
-    height=canvas.height=window.innerHeight;
+    width=canvas.width=400;
+    height=canvas.height=600;
     var ctx=canvas.getContext("2d");
     
-    var myball=new Ball(100,100,40);
-    
+    var myball=new Ball(100,100,50);
+    var striker=new Ball(300,100,70);
     gameLoop();
     function gameLoop(){
         ctx.clearRect(0,0,width,height);
+        myball.collide(striker);
         myball.update();
+        striker.update();
         myball.draw(ctx);
+        striker.draw(ctx);
         requestAnimationFrame(gameLoop);
     }
 };
